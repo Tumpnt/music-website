7757ed854ea05007f25b8006b0ff8d4a8db3d6cd
SArpnt <36542932+SArpnt@users.noreply.github.com>
2019-12-19 08:34:59 -0500

    large cleanup
    there are 2 starts, needs to be fixed

---
+++ sound.js
@@ -0,0 +1,36 @@
+var context
+var songpitch = 440
+var playingnotes = []
+
+function start() {
+	context = new AudioContext()
+	console.log("test")
+}
+
+function updatenotes(start, stop) {
+	for (let i in stop) {
+		var notetodelete
+		for (let ii in playingnotes) {
+			if (playingnotes[ii].note == stop[i]) {
+				notetodelete = playingnotes[ii].sound
+				playingnotes.splice(ii, 1)
+
+				break
+			}
+		}
+		if (notetodelete) {
+			notetodelete.stop()
+		}
+	}
+	for (let i in start) {
+		var pitch = (2 ** ((start[i] - 57) / 12)) * songpitch
+		console.log(pitch)
+		var nn = context.createOscillator()
+		nn.connect(context.destination)
+		nn.type = "sine"
+		nn.frequency.setTargetAtTime(pitch, context.currentTime, 0)
+		nn.start()
+
+		playingnotes.unshift({ sound: nn, note: start[i] })
+	}
+}
