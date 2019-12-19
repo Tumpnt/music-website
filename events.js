18d39500b5f2e11eac62f6bca83fe8fb533c1112
SArpnt <36542932+SArpnt@users.noreply.github.com>
2019-12-19 18:54:19 -0500

    formatting
    stop using spaces as tabs it's ugly and bad

--- events.js
+++ events.js
@@ -1,34 +1,34 @@
 class EventHandler {
-    constructor() {
-        this.listeners = {};
-    }
+	constructor() {
+		this.listeners = {}
+	}
 
-    addEventListener = function(type, callback) {
-        if (!(type in this.listeners)) {
-            this.listeners[type] = [];
-        }
-        this.listeners[type].push(callback);
-    }
-    removeEventListener = function(type, callback) {
-        if (!(type in this.listeners)) {
-            return;
-        }
-        var stack = this.listeners[type];
-        for (var i = 0, l = stack.length; i < l; i++) {
-            if (stack[i] === callback) {
-                stack.splice(i, 1);
-                return;
-            }
-        }
-    }
-    dispatchEvent = function(event, ...params) {
-        if (!(event in this.listeners)) {
-            return true;
-        }
-        var stack = this.listeners[event].slice();
+	addEventListener = function (type, callback) {
+		if (!(type in this.listeners)) {
+			this.listeners[type] = []
+		}
+		this.listeners[type].push(callback)
+	}
+	removeEventListener = function (type, callback) {
+		if (!(type in this.listeners)) {
+			return
+		}
+		var stack = this.listeners[type]
+		for (var i = 0, l = stack.length; i < l; i++) {
+			if (stack[i] === callback) {
+				stack.splice(i, 1)
+				return
+			}
+		}
+	}
+	dispatchEvent = function (event, ...params) {
+		if (!(event in this.listeners)) {
+			return true
+		}
+		var stack = this.listeners[event].slice()
 
-        for (var i = 0, l = stack.length; i < l; i++) {
-            stack[i].call(this, ...params);
-        }
-    };
+		for (var i = 0, l = stack.length; i < l; i++) {
+			stack[i].call(this, ...params)
+		}
+	};
 }
