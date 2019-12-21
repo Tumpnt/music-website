885b448d28f825d5dac0cdc5dcb6459cf014f5d4
Tumble <tumblegamer@gmail.com>
2019-12-21 19:46:16 +0000

    rete

---
+++ node-editor.js
@@ -0,0 +1,45 @@
+/**
+ * Create Editor
+ */
+//create instance
+const numSocket = new Rete.Socket('Number value');
+
+class NumComponent extends Rete.Component {
+    constructor() {
+        super('Number');
+    }
+
+    builder(node) {
+        let out = new Rete.Output('num', 'number', numSocket);
+
+        node.addOutput(out);
+    }
+
+    worker(node, inputs, outputs) {
+        outputs['num'] = node.data.num;
+    }
+}
+
+
+
+/**
+ * Initialise Editor
+ */
+sarpntEventHandler.addEventListener('start', function() {
+    const container = document.querySelector('#rete');
+    const editor = new Rete.NodeEditor('demo@0.1.0', container);
+
+    editor.use(ConnectionPlugin.default)
+    editor.use(VueRenderPlugin)
+
+    const numComponent = new NumComponent();
+    editor.register(numComponent);
+
+    const engine = new Rete.Engine('demo@0.1.0');
+    engine.register(numComponent);
+
+    editor.on('process nodecreated noderemoved connectioncreated connectionremoved', async() => {
+        await engine.abort();
+        await engine.process(editor.toJSON());
+    });
+})
