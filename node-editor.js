51169df8cb4d4c3ace5acac6609ce293bee9676b
Tumble <tumblegamer@gmail.com>
2019-12-21 22:42:07 +0000

    one line

--- node-editor.js
+++ node-editor.js
@@ -5,58 +5,58 @@
 const numSocket = new Rete.Socket('Number value');
 
 class NumComponent extends Rete.Component {
-    constructor() {
-        super('Number');
-    }
+	constructor() {
+		super('Number');
+	}
 
-    builder(node) {
-        let out = new Rete.Output('num', 'Number', numSocket);
+	builder(node) {
+		let out = new Rete.Output('num', 'Number', numSocket);
 
-        node.addOutput(out);
-    }
+		node.addOutput(out);
+	}
 
-    worker(node, inputs, outputs) {
-        outputs['num'] = node.data.num;
-    }
+	worker(node, inputs, outputs) {
+		outputs['num'] = node.data.num;
+	}
 }
 
 
 /**
  * Initialise Editor
  */
-sarpntEventHandler.addEventListener('start', async function() {
-    const container = document.querySelector('#rete');
-    const components = [new NumComponent()];
+sarpntEventHandler.addEventListener('start', async function () {
+	const container = document.querySelector('#rete');
+	const components = [new NumComponent()];
 
 
-    const editor = new Rete.NodeEditor('demo@0.1.0', container);
-    editor.use(ConnectionPlugin.default)
-    editor.use(VueRenderPlugin.default)
+	const editor = new Rete.NodeEditor('demo@0.1.0', container);
+	editor.use(ConnectionPlugin.default)
+	editor.use(VueRenderPlugin.default)
 
 
-    const engine = new Rete.Engine('demo@0.1.0');
-    components.map(c => {
-        editor.register(c);
-        engine.register(c);
-    })
+	const engine = new Rete.Engine('demo@0.1.0');
+	components.map(c => {
+		editor.register(c);
+		engine.register(c);
+	})
 
-    //preset Nodes
-    var n1 = await components[0].createNode({ num: 2 });
+	//preset Nodes
+	var n1 = await components[0].createNode({ num: 2 });
 
-    var loop = () => {
-        n1.position = [80, 200];
-        requestAnimationFrame(loop);
-    }
-    requestAnimationFrame(loop);
-    n1.position = [80, 200];
-    editor.addNode(n1);
+	var loop = () => {
+		n1.position = [80, 200];
+		requestAnimationFrame(loop);
+	}
+	requestAnimationFrame(loop);
+	n1.position = [80, 200];
+	editor.addNode(n1);
 
-    editor.on('process nodecreated noderemoved connectioncreated connectionremoved', async() => {
-        console.log('process');
-        await engine.abort();
-        await engine.process(editor.toJSON());
-    });
+	editor.on('process nodecreated noderemoved connectioncreated connectionremoved', async () => {
+		console.log('process');
+		await engine.abort();
+		await engine.process(editor.toJSON());
+	});
 
-    editor.view.resize();
-    editor.trigger('process');
+	editor.view.resize();
+	editor.trigger('process');
 })
