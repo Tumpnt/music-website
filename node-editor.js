26f7555fcddab16d636faa920ebefc6e93bf9dfe
SArpnt <36542932+SArpnt@users.noreply.github.com>
2019-12-23 09:23:50 -0500

    colored sockets and node removal
    note to pitch was kinda a bad idea

--- node-editor.js
+++ node-editor.js
@@ -10,10 +10,12 @@ class InComponent extends Rete.Component {
 
 	builder(node) {
 		node.addOutput(new Rete.Output('note', 'Note', numSocket))
+		node.addOutput(new Rete.Output('pitch', 'Pitch', numSocket))
 	}
 
 	worker(node, inputs, outputs) {
 		outputs['note'] = 0
+		outputs['pitch'] = 0 //(2 ** ((outputs['note'] - 57) / 12)) * songpitch
 	}
 }
 
@@ -29,21 +31,6 @@ class OutComponent extends Rete.Component {
 	worker(node, inputs, outputs) {
 		if (inputs['sound'])
 			inputs['sound'].connect(context.destination)
-	}
-}
-
-class PitchComponent extends Rete.Component {
-	constructor() {
-		super('Note to Pitch')
-	}
-
-	builder(node) {
-		node.addOutput(new Rete.Output('pitch', 'Pitch', numSocket))
-		node.addInput(new Rete.Input('note', 'Note', numSocket))
-	}
-
-	worker(node, inputs, outputs) {
-		outputs['pitch'] = (2 ** ((inputs['note'] - 57) / 12)) * songpitch
 	}
 }
 
@@ -68,7 +55,7 @@ class OscComponent extends Rete.Component {
 const container = document.querySelector('#rete')
 const editor = new Rete.NodeEditor('demo@0.1.0', container)
 var nodepages = []
-const components = [new InComponent(), new OutComponent(), new PitchComponent, new OscComponent()]
+const components = [new InComponent(), new OutComponent(), new OscComponent()]
 
 async function newNode(t, p, x, y) { //type, page, x,y
 	var tn = await components[t].createNode()
@@ -93,11 +80,9 @@ sarpntEventHandler.addEventListener('start', async function () {
 	//preset Nodes
 	await newNode(0, 0, 0, 0)
 	await newNode(2, 0, 150, 0)
-	await newNode(3, 0, 300, 0)
-	await newNode(1, 0, 450, 0)
-	editor.connect(nodepages[0][0].outputs.get('note'),nodepages[0][1].inputs.get('note'))
-	editor.connect(nodepages[0][1].outputs.get('pitch'),nodepages[0][2].inputs.get('pitch'))
-	editor.connect(nodepages[0][2].outputs.get('sound'),nodepages[0][3].inputs.get('sound'))
+	await newNode(1, 0, 300, 0)
+	editor.connect(nodepages[0][0].outputs.get('pitch'),nodepages[0][1].inputs.get('pitch'))
+	editor.connect(nodepages[0][1].outputs.get('sound'),nodepages[0][2].inputs.get('sound'))
 
 	editor.on('process nodecreated noderemoved connectioncreated connectionremoved', async () => {
 		await engine.abort()
